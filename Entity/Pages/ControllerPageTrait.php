8277690357f1ff13a4c2e684c260766c240d3626
Henrypenny <henrypenny@hotmail.com>
2016-11-14 13:10:37 +1300

    asdf

---
+++ Entity/Pages/ControllerPageTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Hmp\KumaExtraBundle\Entity\Pages;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Kunstmaan\NodeBundle\Controller\SlugActionInterface;
+use Kunstmaan\NodeBundle\Entity\AbstractPage;
+use Kunstmaan\NodeSearchBundle\Helper\SearchTypeInterface;
+use Kunstmaan\PagePartBundle\Helper\HasPageTemplateInterface;
+use Kunstmaan\PagePartBundle\PagePartAdmin\PagePartAdminConfiguratorInterface;
+use Kunstmaan\PagePartBundle\PageTemplate\PageTemplateInterface;
+
+trait ControllerPageTrait
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="controller_action", type="text", nullable=true)
+     */
+    protected $controllerAction;
+
+    public function getControllerAction()
+    {
+        return $this->controllerAction?$this->controllerAction:'KunstmaanNodeBundle:Slug:slug';
+    }
+
+    public function setControllerAction($controllerAction)
+    {
+        $this->controllerAction = $controllerAction;
+    }
+}
